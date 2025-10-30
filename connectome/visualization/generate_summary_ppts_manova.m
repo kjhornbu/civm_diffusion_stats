0bca9d8ae135735c99479bee4910fe0a6f4517e2
kjhornbu <kjhornbu@users.noreply.github.com>
2025-10-30 17:03:05 -0400

    starting automatic report generation file for manova side of pipeline

---
+++ connectome/visualization/generate_summary_ppts_manova.m
@@ -0,0 +1,78 @@
+function [] = generate_summary_ppts_manova(Path_table,project_id,user,connectome_type,pval_threshold,study_model)
+import mlreportgen.ppt.*;
+
+
+connectome_list=list2cell('Unscaled_Omni_Manova BrainScaled_Omni_Manova');
+
+
+%just have Figures for general....
+fig_dir_name='figures';
+
+if strcmp(pvalue_type,'pval_BH')
+    fig_dir_name='figures';
+elseif strcmp(pvalue_type,'pval')
+    fig_dir_name='figures_withoutFDR';
+else
+    keyboard;
+end
+
+if ~istable(Path_table)
+    Path_table=civm_read_table(Path_table);
+end
+
+
+
+    if strcmp(Path_table.stratification{n},'-')
+        ppt_identity={project_id,scallng(),study_model};
+    else
+        ppt_identity={project_id,scallng(),study_model,Path_table.stratification{n}};
+    end
+
+
+    %% Start Powerpoint
+    f_info=dir(interesting_data_path);
+    if ~isempty(f_info)
+        summary_date=datetime(f_info.date);
+    else
+        summary_date=now;
+    end
+
+    ppt_name_components={project_id,'Summary_MANOVA',datestr(summary_date,'yyyy-mm-dd')};
+    scalar_analysis_ppt_summary_file=fullfile(summary_dir,[strjoin(ppt_name_components,'_'), '.pptx']);
+
+    if file_time_check(scalar_analysis_ppt_summary_file,'newer',interesting_data_path)
+        %if exist(scalar_analysis_ppt_summary_file,'file')
+        warning('Previously complete, not updating %s',scalar_analysis_ppt_summary_file);
+        continue;
+    end
+
+    code_dir=fileparts(mfilename('fullpath'));
+    template_file=fullfile(code_dir,'mlreportgen_scalar_analysis.pptx');
+    if exist(template_file,'file')
+        ppt = Presentation(scalar_analysis_ppt_summary_file,template_file);
+    else
+        warning('Missing template %s, using default',template_file);
+        ppt = Presentation(scalar_analysis_ppt_summary_file);
+    end
+    open(ppt);
+
+
+    %% Add Title Slide
+    [slidepointer] = title_slide_setup(ppt,ppt_identity,user);
+
+    %% Global
+    %% Add in Global Results -- Pval -> Make an MANOVA Table?
+
+    %% Add in Global Results -- MDS plot?
+
+    for sov=1:n
+        %% Regional
+        %% Add in Regional Results -- Pval BH
+
+        %% Add in Regional Results -- 1 remove?
+
+        %% list off regions that have 1 remove at highest level?
+    end
+end
+
+end
